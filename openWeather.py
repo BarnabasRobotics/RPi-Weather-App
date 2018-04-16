808e8c6da8fa0e122b6af1ac3aa2f6d7580d5909
Vaneh <vanehboghosian@gmail.com>
2018-04-16 22:25:18 +0000

    Cleaned up the code

--- openWeather.py
+++ openWeather.py
@@ -6,21 +6,13 @@ observation= owm.weather_at_place("North Glendale, US")
 
 w= observation.get_weather()
 
-#wind= w.get_wind()
+wind= w.get_wind()
 
 temperature= w.get_temperature('fahrenheit')
-#print (temperature.split(','))
-for line in temperature:
-    Type= line.split(",")
-    x= Type[1]
-    y= Type[2]
-    print(x,y)
-#tomorrow= pyowm.timeutils.tomorrow()
-#search the error name, stack overflow has possible solution
 
 
-#print(w)
-#print(wind)
-#print(temperature)
-#print(tomorrow)
+print(w)
+print(wind)
+print(temperature)
+
 
